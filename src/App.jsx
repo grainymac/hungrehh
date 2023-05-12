3166836131f1244722e69ece9c2e09a514849457
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-05-12 18:48:50 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [food, setFood] = useState(0)
+
+  return (
+    <>
+    <h1>goin hungrehh?</h1>
+    <h2>select from the drop down list</h2>
+    </>
+  )
+}
+
+export default App
